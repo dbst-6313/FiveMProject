073452453fd903b4aac50c35d8610fe8c59a8add
Furkann35 <yucelfurkan124@gmail.com>
2021-08-05 21:57:24 +0300

    .

--- DataAccess/Concrete/EntityFramework/EfAnswerDal.cs
+++ DataAccess/Concrete/EntityFramework/EfAnswerDal.cs
@@ -109,7 +109,6 @@ namespace DataAccess.Concrete.EntityFramework
                                  StateId = s.id
                              };
                 return result.Where(a => a.StateId == 1).ToList();
-                //test
             }
         }
     }
